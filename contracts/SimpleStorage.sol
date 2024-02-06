b8a60edc2ae0dee8820b37f43c54055320f3fe69
signor1 <emmanuelomemgboji@gmail.com>
2024-02-06 04:49:21 +0100

    feat: updated contract

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -2,12 +2,15 @@
 pragma solidity ^0.8.0;
 
 contract SimpleStorage{
+    //state variable
     uint256 private storedData;
 
+    //the setter function that updates the value of storedData
     function set(uint _value) public {
         storedData = _value;
     }
 
+    //the getter function that returns the value of the storedData
     function get() public view returns(uint256){
         return storedData;
     }
